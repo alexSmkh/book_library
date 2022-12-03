14b65a7cd7041a6c7555eee54c6d9099d273bde6
Alexander Samokhvalov <alexshikari9333@gmail.com>
2022-12-03 20:41:00 +0300

    Added paginator

--- render_website.py
+++ render_website.py
@@ -6,6 +6,10 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 from more_itertools import chunked
 
 
+def build_relative_path_to_file(path):
+    return os.path.relpath(path, 'pages')
+
+
 def init_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -23,17 +27,26 @@ def on_reload(books):
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html'])
     )
+    env.filters['build_relative_path_to_file'] = build_relative_path_to_file
 
     template = env.get_template('template.html')
     book_per_page = 10
-    book_per_line = 2
+    book_per_row = 2
     pages_path = 'pages'
     chunked_books_for_pages = list(chunked(books, book_per_page))
+    page_count = len(chunked_books_for_pages)
+
     os.makedirs(pages_path, exist_ok=True)
 
-    for page_number, book_for_page in enumerate(chunked_books_for_pages, start=1):
-        chunked_books = list(chunked(books, book_per_line))
-        rendered_page = template.render(chunked_books=chunked_books)
+    for page_number, books_for_current_page in enumerate(chunked_books_for_pages, start=1):
+        chunked_books = list(chunked(books_for_current_page, book_per_row))
+        rendered_page = template.render(
+            chunked_books=chunked_books,
+            previous_page=(page_number - 1 if page_number > 1 else None),
+            next_page=(page_number + 1 if page_number < page_count else None),
+            page_number=page_number,
+            page_count=page_count
+        )
 
         with open(os.path.join(pages_path, f'{page_number}_index.html'), 'w', encoding='utf8') as file:
             file.write(rendered_page)
@@ -48,6 +61,7 @@ def main():
         books = json.load(file)
 
     on_reload(books)
+
 
 
 if __name__ == '__main__':
